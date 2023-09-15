41b61238243be38fdcbc440af0b6854198b50f4c
yassarmed <yassarahmed17@gmail.com>
2023-09-14 19:37:08 -0500

    adds products index

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,11 @@
+import { ProductsIndex } from "./ProductsIndex"
+
+
+
 export function Content() {
   return (
     <div>
-      <h1>Welcome to React!</h1>
+    <ProductsIndex/>
     </div>
   )
 }
---
+++ src/ProductsIndex.jsx
@@ -0,0 +1,7 @@
+export function ProductsIndex() {
+  return (
+    <div>
+      <h1>All Products</h1>
+    </div>
+  );
+}
