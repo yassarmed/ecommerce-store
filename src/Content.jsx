e5488f8c546d48bfd5604d83a69d05d20ef037b8
yassarmed <yassarahmed17@gmail.com>
2023-09-17 12:21:41 -0500

    adds modal and fetches api

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,42 @@
-import { ProductsIndex } from "./ProductsIndex"
+// Content.jsx
 
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { ProductsIndex } from "./ProductsIndex";
+import { Modal } from "./Modal";
 
 export function Content() {
+  const [products, setProducts] = useState([]);
+  const [isProductsShowVisible, setIsProductsShowVisible] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState({});
+
+  const handleIndexProducts = () => {
+    console.log("handleIndexProducts");
+    axios.get("https://fakestoreapi.com/products").then((response) => {
+      console.log(response.data);
+      setProducts(response.data);
+    });
+  };
+
+  const handleShowProduct = (product) => {
+    console.log("handleShowProduct", product);
+    setIsProductsShowVisible(true);
+    setCurrentProduct(product);
+  };
+
+  const handleClose = () => {
+    console.log("handleClose");
+    setIsProductsShowVisible(false);
+  };
+
+  useEffect(handleIndexProducts, []);
+
   return (
     <div>
-    <ProductsIndex/>
+      <ProductsIndex products={products} onShowProduct={handleShowProduct} />
+      <Modal show={isProductsShowVisible} onClose={handleClose}>
+        <h1>Test</h1>
+      </Modal>
     </div>
-  )
+  );
 }
--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,7 +1,17 @@
-export function ProductsIndex() {
+export function ProductsIndex(props) {
   return (
     <div>
       <h1>All Products</h1>
+      {props.products.map((product) => (
+         <div key={product.id}>
+           <h2>{product.name}</h2>
+           <img src={product.image} width={60} />
+           <p>{product.description}</p>
+           <p> {product.category}</p>
+           <button onClick={() => props.onShowProduct(product)}>More info</button>
+         </div>
+       ))}
+
     </div>
   );
 }
